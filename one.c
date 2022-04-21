7b7aa4c199f9b43662693a85011619a0cd69882b
wenjianlei <864755286@qq.com>
2022-04-21 19:43:03 +0800

    bei_zhu

--- one.c
+++ one.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
 
+
+// git init
+// git add one.c
+// git commit -m ".." one.c
+// git reset --hard 1c174cc
+// git rm --cached one.c
+// git branch hot-fix
+// git branch -v
+// git checkout hot-fix
+// git mergr hot-fix
+// git commit -m "merge"
+// push clone pull fork (pull request) merge
+// git remote -v
+// git push origin master
+// git pull origin master
+// git@github.com:HighGrooce/studious-memory.git
+
 int main()
 {
     printf("hello world\n");
